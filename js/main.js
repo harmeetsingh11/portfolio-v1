970689cde1caf386901f2474e4d7e52117f785e0
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-07-26 21:22:08 +0530

    feat: implement show functionality of nav menu

---
+++ js/main.js
@@ -0,0 +1,41 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu');
+navToggle = document.getElementById('nav-toggle');
+navClose = document.getElementById('nav-close');
+
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+/*==================== ACCORDION SKILLS ====================*/
+
+/*==================== QUALIFICATION TABS ====================*/
+
+/*==================== SERVICES MODAL ====================*/
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+/*==================== TESTIMONIAL ====================*/
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+
+/*==================== SHOW SCROLL UP ====================*/
+
+/*==================== DARK LIGHT THEME ====================*/
